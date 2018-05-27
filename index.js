6494e469744649ed75be842901d9bdb7213aecd6
wkdewey@gmail.com <wkdewey@gmail.com>
2018-05-27 18:45:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var strings = []
-  for (i = 0; i < musicians.length; i++) {
+  for (let i = 0; i < musicians.length; i++) {
     strings.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return strings
